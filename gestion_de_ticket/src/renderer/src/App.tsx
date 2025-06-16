67725cfedc1e5b599d5cf6dbf290c74fac5bca77
Abdelkader-Kahloune <abdelkaderkahloun2003@gmail.com>
2025-06-16 13:44:30 +0100

    adding login page

---
+++ gestion_de_ticket/src/renderer/src/App.tsx
@@ -0,0 +1,13 @@
+import Login from './Login'
+
+function App(): React.JSX.Element {
+  //const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
